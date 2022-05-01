f9e5805e00b50aec2d04c6b51606818c1dbd8af5
Mridul Dey <mridul.dey@ellucian.com>
2022-05-01 12:20:59 +0530

    search insert position

--- Binary-Search/BS_leetcode.js
+++ Binary-Search/BS_leetcode.js
@@ -114,3 +114,76 @@ var guessNumber = function (n) {
 
   return guessedNum(1, n);
 };
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let mid;
+
+  const search = (start, end) => {
+    if (start <= end) {
+      mid = Math.floor((start + end) / 2);
+
+      if (nums[mid] === target) {
+        return mid;
+      }
+
+      if (nums[mid] > target) {
+        return search(start, mid - 1);
+      }
+
+      return search(mid + 1, end);
+    }
+
+    // if target not in range of nums and
+    if (target < nums[0]) {
+      return 0;
+    }
+
+    // if target not in range of nums and
+    if (target > nums[nums.length - 1]) {
+      return nums.length;
+    }
+
+    // if target in range of nums and
+    if (nums[mid] > target) {
+      return mid;
+    } else {
+      return mid + 1;
+    }
+  };
+
+  return search(0, nums.length - 1);
+};
