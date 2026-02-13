475d8ef82c9df24b7778e2cec672ce0f71083549
mriduldey <mriduldey94@gmail.com>
2026-02-14 03:06:23 +0530

    BS > improved rotated sorted array BS added

--- Algo/Binary-Search/3.rotated-array-search.js
+++ Algo/Binary-Search/3.rotated-array-search.js
@@ -1,62 +1,30 @@
-// max item is concedered as pivot
-function findPivot(arr, start, end) {
-  if (start < end) {
-    const mid = Math.floor((start + end) / 2);
+function binarySearchRotated (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-    // max item found when next element is smaller
-    if (arr[mid] > arr[mid + 1]) {
-      return mid;
+  while(left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if(nums[mid] === target) return mid;
+
+    if(nums[left] <= nums[mid]) {
+      // Left half sorted
+      if(nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // Right half sorted
+      if(nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
-
-    // min item is found when previous item is greater |
-    // previous is the max item
-    if (arr[mid] < arr[mid - 1]) {
-      return mid - 1;
-    }
-
-    if (arr[start] >= arr[mid]) {
-      return findPivot(arr, start, mid - 1);
-    }
-
-    return findPivot(arr, mid + 1, end);
-  } else {
-    return -1;
-  }
-}
-
-function binarySearch(arr, start, end, target) {
-  console.log('start', start, 'end', end);
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    console.log('mid', mid, 'value', arr[mid]);
-    if (arr[mid] === target) {
-      return mid;
-    }
-
-    if (arr[mid] > target) {
-      end = mid - 1;
-      continue;
-    }
-
-    start = mid + 1;
   }
 
   return -1;
 }
 
-function findTarget(arr, target) {
-  const start = 0,
-    end = arr.length - 1;
-
-  const pivotIndex = findPivot(arr, start, end);
-
-  if (pivotIndex !== -1) {
-    console.log('pivot index', pivotIndex);
-    if (target > arr[start]) {
-      return binarySearch(arr, start, pivotIndex, target);
-    }
-    return binarySearch(arr, pivotIndex + 1, end, target);
-  }
-}
-
-console.log(findTarget([4, 5, 6, 7, 89, 1, 2], 6));
+console.log(binarySearchRotated([0, 2, 4, 7, 8, 10, 23], 23));
